7240ed79b27308a3e8bb652f80a50b3986b19234
Arthur <65630024+Arthur-nonaka@users.noreply.github.com>
2024-03-22 11:23:26 -0300

    Add files via upload

--- POO_241/Classes/PrivateEmployee.php
+++ POO_241/Classes/PrivateEmployee.php
@@ -1,25 +1,52 @@
 <?php
 
-class Employee {
+class Employee
+{
     private $code;
     private $name;
     private $wage;
 
-    public function GetCode(){
+    public function GetCode()
+    {
         return $this->code;
     }
-    public function SetCode($code){
+    public function SetCode($code)
+    {
         $this->code = $code;
     }
-
-    public function NewEmployee($code, $name, $wage) {
+    public function GetName()
+    {
+        return $this->name;
+    }
+    public function SetName($name)
+    {
+        $this->name = $name;
+    }
+    public function GetWage()
+    {
+        return $this->code;
+    }
+    public function SetWage($wage)
+    {
+        $this->wage = $wage;
     }
 
-    public function RemoveEmployee($code){
-
+    public function NewEmployee($code, $name, $wage)
+    {
+        $this->code = $code;
+        $this->name = $name;
+        $this->wage = $wage;
     }
 
-    public function ShowAttributes() {
-        
+    public function RemoveEmployee()
+    {
+        $this->wage = 0;
+    }
+
+    public function ShowAttributes()
+    {
+        echo "<br> Codigo: " . $this->code;
+        echo "<br> Nome: " . $this->name;
+        echo "<br> Salario: " . $this->wage;
     }
 }
