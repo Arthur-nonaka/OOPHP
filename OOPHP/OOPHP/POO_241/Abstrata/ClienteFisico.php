af291603d8693270ec3fe8db162b2e93c84469ed
Arthur <nonakaodaarthur@gmail.com>
2024-05-17 14:12:50 -0300

    Abstract

--- OOPHP/OOPHP/POO_241/Abstrata/ClienteFisico.php
+++ OOPHP/OOPHP/POO_241/Abstrata/ClienteFisico.php
@@ -1,4 +1,26 @@
 <?php
 class ClienteFisico extends Cliente{
-    
+    private $rg;
+    private $cpf;
+
+    public function setRG($rg){
+        $this->rg = $rg;
+    }
+    public function setCPF($cpf){
+        $this->cpf = $cpf;
+    }
+
+    public function MostarDados(){
+        parent::MostarDados();
+        echo "<br> RG: " . $this->rg . " <br> CPF: ". $this->cpf;
+    }
+
+    public function avaliaIdade() {
+        if($this->idade >= 18 && $this->idade < 40) {
+            echo "<br> Cliente fisico";
+        }
+        else if($this->idade > 40){
+            echo "<br> Cliente Juridico";
+        }
+    }
 }
